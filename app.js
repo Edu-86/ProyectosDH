fa2a5f4d11e494d6ef15d97bf0b7311bf735950d
Edu <manson_eduardoperez@hotmail.com>
2022-04-04 06:34:21 -0300

    Primer comit del Proyecto individual

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
